cc1ac33889465bdf821dc2c74b0156191e95d36c
LeelaPrashanthi <148841261+LeelaPrashanthi@users.noreply.github.com>
2025-06-29 12:57:01 +0530

    Delete Week-2/JUnit_AdvancedTesting directory

--- Week-2/JUnit_AdvancedTesting/src/main/java/ExceptionThrower.java
+++
@@ -1,8 +0,0 @@
-public class ExceptionThrower {
-
-    public void throwIfNegative(int number) {
-        if (number < 0) {
-            throw new IllegalArgumentException("Negative number not allowed");
-        }
-    }
-}
