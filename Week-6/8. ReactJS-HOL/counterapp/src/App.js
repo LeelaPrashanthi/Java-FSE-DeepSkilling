eb1393510fe24e1b9adf8517ca7f28e22106e7f1
LeelaPrashanthi <148841261+LeelaPrashanthi@users.noreply.github.com>
2025-07-27 21:19:48 +0530

    Add files via upload

---
+++ Week-6/8. ReactJS-HOL/counterapp/src/App.js
@@ -0,0 +1,13 @@
+// App.js
+import React from "react";
+import CountPeople from "./CountPeople";
+
+function App() {
+  return (
+    <div className="App">
+      <CountPeople />
+    </div>
+  );
+}
+
+export default App;
