59dd079b34727cfd7307d192ec4442bd2e72d11c
LeelaPrashanthi <148841261+LeelaPrashanthi@users.noreply.github.com>
2025-08-01 11:10:53 +0530

    Add files via upload

---
+++ Week-6/5. ReactJS-HOL - cohorttracker/src/Cohort.js
@@ -0,0 +1,26 @@
+export const CohortsData = [
+  {
+    cohortCode: 'INTADMDF10',
+    technology: '.NET FSD',
+    startDate: '22-Feb-2022',
+    currentStatus: 'Scheduled',
+    coachName: 'Aathma',
+    trainerName: 'Jojo Jose'
+  },
+  {
+    cohortCode: 'ADM21JF014',
+    technology: 'Java FSD',
+    startDate: '10-Sep-2021',
+    currentStatus: 'Ongoing',
+    coachName: 'Apoorv',
+    trainerName: 'Elisa Smith'
+  },
+  {
+    cohortCode: 'CDBJF21025',
+    technology: 'Java FSD',
+    startDate: '24-Dec-2021',
+    currentStatus: 'Ongoing',
+    coachName: 'Aathma',
+    trainerName: 'John Doe'
+  }
+];
